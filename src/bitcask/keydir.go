bb0e47e798013f525971b0c07e46689bc8177a21
myang <myang199088@163.com>
2015-06-14 14:12:39 +0800

    bitcask

---
+++ src/bitcask/keydir.go
@@ -0,0 +1,69 @@
+package bitcask
+
+/*
+   Impletation of keydir
+   | --- |      | -----------------------------------------------------------------------------|
+   | key | -->  | file id (int32) | value size (int32) | value pos (int32) | time stamp (int64) |
+   | --- |      | -----------------------------------------------------------------------------|
+*/
+
+import (
+	"sync"
+)
+
+type item struct {
+	fileId    int32
+	valueSize int32
+	valuePos int32
+	timeStamp int64
+}
+
+// keydir is a index data structure for bitcask
+// It wrap for hashmap(builtin go)
+// It is safe to call add, remove, get concurrently.
+type keydir struct {
+	sync.RWMutex
+	data map[string]item
+}
+
+func NewKeydir() *keydir {
+	return &keydir{
+		data: make(map[string]item),
+	}
+}
+
+func (kd *keydir) add(key string, fid int32, valueSize int32, valuePos int32, timeStamp int64) {
+	kd.Lock()
+	defer kd.Unlock()
+
+	kd.data[key] = item{fid, valueSize, valuePos, timeStamp}
+
+	return
+}
+
+func (kd *keydir) get(key string) (*item, bool) {
+	kd.RLock()
+	defer kd.RUnlock()
+
+	v, b := kd.data[key]
+	return &v, b
+}
+
+func (kd *keydir) delete(key string) {
+	kd.Lock()
+	defer kd.Unlock()
+
+	delete(kd.data, key)
+}
+
+func (kd *keydir) keys() chan string {
+	ch := make(chan string)
+	go func() {
+		for k, _ := range kd.data {
+			ch <- k
+		}
+		close(ch)
+	}()
+	return ch
+}
+
---
+++ src/bitcask/keydir_test.go
@@ -0,0 +1,34 @@
+package bitcask
+
+import (
+	"testing"
+	"fmt"
+)
+
+func Test(t *testing.T) {
+	kd := NewKeydir()
+	kd.add("fvck", 1, 1, 1, 1000)
+	kd.add("fvck1", 1, 1, 1, 1001)
+	it, _ := kd.get("fvck")
+	if it.fileId != 1 && it.timeStamp != 1000 && it.valuePos != 1 && it.valueSize != 1{
+		t.Errorf("get(fvck) failed.")
+	}
+	it, _ = kd.get("fvck1")
+	if it.fileId != 1 && it.timeStamp != 1001 && it.valuePos != 1 && it.valueSize != 1{
+		t.Errorf("get(fvck1) failed.")
+	}
+
+	keysChan := kd.keys()
+	for key := range keysChan {
+		fmt.Println("key:", key)
+	}
+
+	kd.delete("fvck")
+	var ok bool
+	it, ok = kd.get("fvck")
+	if ok {
+		t.Errorf("delete(fvck) failed.")
+	}
+}
+
+
