28b4a1cd02ecfae7d08ff1b2179e085689a9ff11
myang <myang199088@163.com>
2015-06-14 23:12:25 +0800

    bitcask

--- src/bitcask/bitcask.go
+++ src/bitcask/bitcask.go
@@ -1,1 +1,275 @@
 package bitcask
+
+import (
+	"os"
+	"sync"
+	"log"
+	"errors"
+	"fmt"
+	"time"
+	"path"
+	"sort"
+	"io"
+)
+
+const (
+	LOGFILE         string      = "bitcask.log"
+	defaultFilePerm os.FileMode = 0666
+	defaultDirPerm  os.FileMode = 0766
+	DATA_FILE       string      = "%09d.data"
+)
+
+type Stats struct {
+	total int64
+	dead      int64
+	isMerging bool
+}
+
+
+type Options struct {
+	MaxFileSize  int32
+//	MergeWindow  [2]int // startTime-EndTime
+//	MergeTrigger float32
+	Path         string
+}
+
+
+type Bitcask struct {
+	Stats
+	Options
+	sync.Mutex
+	curr *file
+	kd *keydir
+}
+
+
+var ErrKeyNotFound = errors.New("Key not found")
+var Lg *log.Logger
+
+
+//Set log file
+func init() {
+	os.Remove(LOGFILE)
+	logfile, _ := os.OpenFile(LOGFILE, os.O_RDWR|os.O_CREATE, defaultFilePerm)
+	Lg = log.New(logfile, "\n", log.Ldate|log.Ltime|log.Llongfile)
+}
+
+
+func NewBitcask(o Options) (*Bitcask, error) {
+	err := os.MkdirAll(o.Path, defaultDirPerm)
+	if err != nil {
+		return nil, fmt.Errorf("Make dir %s %s", o.Path, err.Error())
+	}
+
+	b := new(Bitcask)
+	b.kd = newKeydir()
+	b.Options = o
+
+	err = b.scan()
+//	go b.merge()
+	return b, err
+}
+
+
+func (b *Bitcask) Close() error {
+	if err := b.Sync(); err != nil {
+		return err
+	}
+	return b.curr.close()
+}
+
+
+func (b *Bitcask) Sync() error {
+	return b.curr.sync()
+}
+
+
+func (b *Bitcask) Set(key string, value []byte) error {
+	b.Lock()
+	defer b.Unlock()
+	err := b.set(key, value, time.Now().Unix())
+	return err
+}
+
+
+func (b *Bitcask) Get(key string) ([]byte, error) {
+	item, ok := b.kd.get(key)
+	if !ok {
+		return nil, ErrKeyNotFound
+	}
+	value, err := b.getValue(item)
+	return value, err
+}
+
+
+func (b *Bitcask) Del(key string) error {
+	b.Lock()
+	defer b.Unlock()
+
+	value := []byte{0}
+	err := b.set(key, value, time.Now().Unix())
+	b.kd.delete(key)
+
+	return err
+}
+
+func (b *Bitcask) Keys() chan string {
+	return b.kd.keys()
+}
+
+
+func (b *Bitcask) Has(key string) bool {
+	_, ok := b.kd.get(key)
+	return ok
+}
+
+
+func (b *Bitcask) set(key string, value []byte, tstamp int64) error {
+	if len(key) == 0 {
+		return fmt.Errorf("Key can not be None")
+	}
+
+	if RECORD_HEADER_SIZE+int32(len(key)+len(value))+b.curr.offset > b.MaxFileSize {
+		if err := b.curr.close(); err != nil {
+			return fmt.Errorf("Close %s failed: %s", b.curr.io.Name(), err.Error())
+		}
+		nextFid := b.curr.id + 1
+		nextPath := path.Join(b.Path, fmt.Sprintf(DATA_FILE, nextFid))
+		nextFp, err := os.OpenFile(nextPath, os.O_CREATE|os.O_APPEND|os.O_RDWR, defaultDirPerm)
+		if err != nil {
+			return fmt.Errorf("Create %s failed :%s", nextFp.Name(), err.Error())
+		}
+		b.curr = newFile(nextFp, nextFid)
+	}
+	vpos, err := b.curr.write(key, value, tstamp)
+	if err != nil {
+		return err
+	}
+
+	if b.Has(key) {
+		b.dead++
+	}
+	b.kd.add(key, b.curr.id, int32(len(value)), vpos, tstamp)
+	b.total++
+
+	return nil
+}
+
+
+func (b *Bitcask) getValue(it *item) ([]byte, error) {
+	fp, err := os.Open(path.Join(b.Path, fmt.Sprintf(DATA_FILE, it.fileId)))
+	if err != nil {
+		return nil, fmt.Errorf("getValue %s", err.Error())
+	}
+	defer fp.Close()
+	value := make([]byte, it.valueSize)
+	realSize, err := fp.ReadAt(value, int64(it.valuePos))
+	if int32(realSize) != it.valueSize {
+		return nil, fmt.Errorf("Expected %d bytes got %d", it.valueSize, realSize)
+	}
+	return value, nil
+}
+
+
+func getFileNames(dirPath string) ([]string, error) {
+	var (
+		dir *os.File
+		err error
+	)
+	if dir, err = os.Open(dirPath); err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+
+	fns, _ := dir.Readdirnames(-1)
+	sort.Strings(fns)
+	return fns, nil
+}
+
+
+func (b *Bitcask) fillKeydir(fn string) error {
+	f, err := os.Open(path.Join(b.Path, fn))
+	if err != nil {
+		return fmt.Errorf("FillKeydir : %s", err.Error())
+	}
+	defer f.Close()
+
+	var fid int32
+	fmt.Sscanf(fn, DATA_FILE, &fid)
+	fl := newFile(f, fid)
+	var (
+		toreturn error
+		offset   int32 = 0
+	)
+	for {
+		r, err := fl.read()
+		if err != nil {
+			if err != io.EOF {
+				toreturn = err
+			}
+			break
+		}
+
+		offset += RECORD_HEADER_SIZE + r.ksz + r.vsz
+		key := string(r.key)
+		if b.isMerging {
+			b.Lock()
+			if it, ok := b.kd.get(key); ok {
+				if r.tstamp == it.timeStamp {
+					b.total--
+					err = b.set(key, r.value, r.tstamp)
+					if err != nil {
+						return fmt.Errorf("Failed to set", err.Error())
+					}
+				}
+			} else {
+				b.dead--
+			}
+			b.Unlock()
+		} else {
+			// valid item
+			if r.vsz != 1 || r.value[0] != 0 {
+				b.total++
+				b.kd.add(key, fid, r.vsz, offset-r.vsz, r.tstamp)
+				if b.Has(key) {
+					b.dead++
+				}
+			} else { // invalid item(delete)
+				b.dead++
+			}
+		}
+	}
+	return toreturn
+}
+
+
+func (b *Bitcask) scan() error {
+	fns, err := getFileNames(b.Path)
+	if err != nil {
+		return err
+	}
+	for _, f := range fns {
+		err := b.fillKeydir(f)
+		if err != nil {
+			return fmt.Errorf("scan fillkeydir : %s", err.Error())
+		}
+	}
+
+	// choose active file
+	var activeFilePath string
+	var fid int32
+	if len(fns) == 0 {
+		activeFilePath = path.Join(b.Path, fmt.Sprintf(DATA_FILE, 0))
+		fid = 0
+	} else {
+		activeFilePath = path.Join(b.Path, fns[len(fns)-1])
+		fid = int32(len(fns)) - 1
+	}
+	Lg.Println("Open activefile " + activeFilePath)
+	var activefile *os.File
+	activefile, err = os.OpenFile(activeFilePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0766)
+	b.curr = newFile(activefile, fid)
+
+	return err
+}
+
--- src/bitcask/file.go
+++ src/bitcask/file.go
@@ -1,1 +1,191 @@
 package bitcask
+
+import (
+	"bufio"
+	"bytes"
+	"compress/zlib"
+	"encoding/binary"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"os"
+)
+
+const (
+	RECORD_HEADER_SIZE int32 = 20
+)
+
+/*
+    Each record is stored in the following format:
+   	|-----------------------------------------------------------------------------------------|
+	|crc (uint32) | tstamp (int64) | ksz(int32) | vsz (int32) | key ([]byte) | value ([]byte) |
+	|-----------------------------------------------------------------------------------------|
+*/
+
+type record struct {
+	crc    uint32
+	tstamp int64
+	ksz    int32
+	vsz    int32
+	key    []byte
+	value  []byte
+}
+
+func (r *record) compress() error {
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	if _, err := w.Write(r.value); err != nil {
+		return fmt.Errorf("compress value : %s", err.Error())
+	}
+	w.Close()
+	r.value = []byte(b.Bytes())
+	r.vsz = int32(len(r.value))
+	return nil
+}
+
+func (r *record) uncompress() error {
+	b := bytes.NewReader(r.value)
+	zr, err := zlib.NewReader(b)
+	if err != nil {
+		return fmt.Errorf("uncompress value %s", err.Error())
+	}
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, zr)
+	r.value = buf.Bytes()
+	zr.Close()
+	return nil
+}
+
+func (r *record) encode() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, r.tstamp)
+	binary.Write(buf, binary.BigEndian, r.ksz)
+	binary.Write(buf, binary.BigEndian, r.vsz)
+	buf.Write(r.key)
+	buf.Write(r.value)
+	crc := crc32.ChecksumIEEE(buf.Bytes())
+
+	buf2 := new(bytes.Buffer)
+	binary.Write(buf2, binary.BigEndian, crc)
+	buf2.Write(buf.Bytes())
+
+	return buf2.Bytes(), nil
+}
+
+
+type file struct {
+	io     *os.File
+	wbuf   *bufio.Writer
+	offset int32
+	id     int32
+}
+
+
+func newFile(f *os.File, id int32) *file {
+	Lg.Println("Create file" + f.Name())
+	fi, _ := f.Stat()
+	offset := fi.Size()
+	return &file{
+		io:     f,
+		wbuf:   bufio.NewWriter(f),
+		offset: int32(offset),
+		id:     id}
+}
+
+
+func (f *file) write(key string, value []byte, tstamp int64) (int32, error) {
+	r := &record{
+		key:    []byte(key),
+		value:  value,
+		ksz:    int32(len(key)),
+		vsz:    int32(len(value)),
+		tstamp: tstamp,
+	}
+
+	var pos int32
+	var err error
+	if pos, err = f.writeRecord(r); err != nil {
+		return -1, err
+	}
+
+	return pos, nil
+}
+
+
+func (f *file) writeRecord(r *record) (int32, error) {
+	data, err := r.encode()
+	if err != nil {
+		return -1, err
+	}
+	sz, err := f.wbuf.Write(data)
+	if err != nil {
+		return -1, err
+	}
+	if sz < len(data) {
+		err = fmt.Errorf("writeRecord: expected %d got %d\n", len(data), sz)
+		return -1, err
+	}
+
+	valuePos := int32(f.offset + RECORD_HEADER_SIZE + int32(len(r.key)))
+	f.offset += int32(sz)
+	Lg.Println("write %s to %s", string(r.key), f.io.Name())
+	return valuePos, nil
+}
+
+func (f *file) sync() error {
+	if err := f.wbuf.Flush(); err != nil {
+		return err
+	}
+	e := f.io.Sync()
+	return e
+}
+
+func (f *file) read() (*record, error) {
+	r := new(record)
+	headerData := make([]byte, RECORD_HEADER_SIZE)
+
+	var sz int
+	var err error
+	if sz, err = f.io.Read(headerData); err != nil {
+		return nil, err
+	}
+	if int32(sz) != RECORD_HEADER_SIZE {
+		return nil, fmt.Errorf("Read Header: exptectd %d, got %d", RECORD_HEADER_SIZE, sz)
+	}
+	buf := bytes.NewReader(headerData)
+	binary.Read(buf, binary.BigEndian, &r.crc)
+	binary.Read(buf, binary.BigEndian, &r.tstamp)
+	binary.Read(buf, binary.BigEndian, &r.ksz)
+	binary.Read(buf, binary.BigEndian, &r.vsz)
+	r.key = make([]byte, r.ksz)
+	r.value = make([]byte, r.vsz)
+	if _, err := f.io.Read(r.key); err != nil {
+		return nil, fmt.Errorf("key: %s", err.Error())
+	}
+	if _, err := f.io.Read(r.value); err != nil {
+		return nil, fmt.Errorf("Read value: %s", err.Error())
+	}
+
+	//check crc
+	data := append(append(headerData, r.key...), r.value...)
+	crc := crc32.ChecksumIEEE(data[4:])
+	if r.crc != crc {
+		return nil, fmt.Errorf("CRC check failed %u %u", r.crc, crc)
+	}
+	return r, err
+}
+
+func (f *file) name() string {
+	if f.io != nil {
+		return f.io.Name()
+	}
+	return ""
+}
+
+func (f *file) close() error {
+	if err := f.wbuf.Flush(); err != nil {
+		return err
+	}
+	return f.io.Close()
+}
+
